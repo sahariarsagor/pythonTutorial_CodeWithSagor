a5bec09c52366f2a0cc45a292a21b9f9e3f336a2
Sahariar Sagor <contactsahariar@gmail.com>
2026-05-12 23:10:59 +0600

    Add files via upload

---
+++ List/01_List.py
@@ -0,0 +1,9 @@
+# student = ["sagor", 21, 3.25, "Dhaka"]
+
+# student[2] = 3.58
+# print(student)
+
+numbers = [20, 30, 40, 50, 60]
+# numbers.append(70)
+numbers.insert(3, 70)
+print(numbers)
---
+++ List/02_list.py
@@ -0,0 +1,38 @@
+# numbers = [10, 20]
+# numbers.extend([30,40,50])
+# print(numbers)
+
+# fruits = ["mango", "banana", "orange"]
+# fruits.remove("banana")
+# print(fruits)
+
+# num = [10,20,30,40,50]
+# num.pop(2)
+# print(num)
+
+# num = [10, 30, 20, 40]
+# num.clear()
+# print(num)
+
+# fruits = ["banana", "mango", "orange", "apple"]
+# searchingElement = fruits.index("apple")
+# print(searchingElement)
+
+# n = [10, 20, 30, 10, 25, 10, 35, 10]
+# # print(n.count(10))
+
+# n.sort()
+# print(n)
+
+# n = [1, 3, 4, 5]
+# n.reverse()
+# print(n)
+
+# m = n.copy()
+# print(m)
+
+n = [1, 3, 4, 5]
+# print(len(n))
+# print(sum(n))
+# print(max(n))
+print(min(n))
